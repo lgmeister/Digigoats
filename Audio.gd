dd3b4a0e6d85bc19938b1fd52afe993a0ab89874
lgmeister <72280298+lgmeister@users.noreply.github.com>
2023-05-11 15:11:43 -0700

    Completed Initial

--- Audio.gd
+++ Audio.gd
@@ -4,39 +4,52 @@ extends Control
 onready var playlist_scene = load("res://scenes/main/Playlist.tscn")
 
 ### Nodes ###
-var playlist
+var playlist_node
+var music_player_node
+
 onready var boost_node
 
 onready var music = $Music
 onready var sfx = $SFX
 onready var movement = $Movement
 
-var main_volume = 5
-var sfx_volume = 5
-var music_volume = 5
+var main_volume = 1
+var sfx_volume = .5
+var music_volume = .5
+
+### Misc ###
+var fade_out_time = 1
+
+
 
 ### Bools ###
 var boosting = false
 
+### Misc ###
+var music_playlist = []
+
 
 func _ready():
 	print("Audio Player Active")
+	
 	load_playlist()
 	
 	
 	### Put into Settings ###
 	AudioServer.set_bus_mute(0,false)
 	AudioServer.set_bus_volume_db(0,0)
+	
 	sfx.volume_db = -6
 	movement.volume_db = -6
 	music.volume_db = -6
 	
 	
-
 func load_playlist():
 	var scene = playlist_scene.instance()
-	playlist = scene
+	playlist_node = scene
 	add_child(scene)
+	music_playlist = playlist_node.make_playlist()
+
 	
 func change_volume(type,amount):
 	var db = linear2db(amount)
@@ -49,9 +62,11 @@ func change_volume(type,amount):
 			AudioServer.set_bus_mute(0,false)
 			AudioServer.set_bus_volume_db(0,db)
 	elif type == "music":
+		music_volume = amount
 		if amount == 0: music.volume_db = -80
 		else: music.volume_db = db
 	elif type == "sfx":
+		sfx_volume = amount
 		if amount == 0:
 			sfx.volume_db = -80
 			movement.volume_db = -80
@@ -59,11 +74,23 @@ func change_volume(type,amount):
 			sfx.volume_db = db
 			movement.volume_db = db
 			
+func fade_music(type):
+	var tween = get_tree().create_tween()
+	if type == "inout":
+		tween.tween_property(music,"volume_db",-40,fade_out_time)
+		tween.tween_property(music,"volume_db",linear2db(music_volume),.5)
+	elif type == "out":
+		tween.tween_property(music,"volume_db",-40,fade_out_time)
+	elif type == "in":
+		tween.tween_property(music,"volume_db",linear2db(music_volume),.5)
+	elif type == "immediate_in":
+		tween.tween_property(music,"volume_db",linear2db(music_volume),.1)
+		
 
 func new_player(type,file):
 	var audio_node = AudioStreamPlayer.new()
 	add_child(audio_node)
-	audio_node.stream = playlist.get(type)[file]
+	audio_node.stream = playlist_node.get(type)[file]
 	if file == "boost": boost_node = audio_node
 	audio_node.play()
 	yield(audio_node,"finished")
@@ -74,18 +101,38 @@ func new_player(type,file):
 
 	
 func play(file):
-	var type = playlist.check_type(file)
+	var type = playlist_node.check_type(file)
 	
 	if file == "boost":
 		boosting = true
 		new_player(type,file)
 		return
+		
+	if file == "music":
+		fade_music("in")
 	
-	if get(type).playing:
+	if get(type).playing and type != "music":
 		new_player(type,file)
+	elif get(type).playing and type == "music":
+		music_player_node.music_playing = false
+		fade_music("out")
+		yield(get_tree().create_timer(fade_out_time),"timeout")
+		fade_music("immediate_in")
+		get(type).stream = playlist_node.get(type)[file]
+		music_player_node.music_playing = true
+		get(type).play()
 	else:
-		get(type).stream = playlist.get(type)[file]
+		get(type).stream = playlist_node.get(type)[file]
 		get(type).play()
 		
+func pause(type):
+	if type == "music":
+		music.stream_paused = true
+	
+func resume(type):
+	if type == "music":
+		music.stream_paused = false	
 		
 
+func _on_Music_finished():
+	music_player_node.play_next()
--- MusicPlayer.gd
+++ MusicPlayer.gd
@@ -8,22 +8,34 @@ onready var song_image = $Panel/SongImage
 onready var play_button = $Panel/PlayButton
 onready var song_progress = $Panel/SongProgress
 onready var animation = $AnimationPlayer
+onready var song_length = $Panel/SongLength
+onready var song_time = $Panel/SongTime
+
 
 ### Bools ###
 var expanded = false
 var mouse_inside = false
+var music_playing = false
+var paused = false
+
 
 ### Music ###
 var music_playlist
 
+var song_index = -1
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	AUDIO.music_playlist = music_playlist
-	print(music_playlist)
+	AUDIO.music_player_node = self
+	
+	music_playlist = AUDIO.music_playlist 
+	music_playlist.shuffle()
+	
+	play_next()
+	
 
 
-func _process(_delta):
+func _process(delta):
 	if get_local_mouse_position().y < 0\
 	or get_local_mouse_position().x > self.rect_size.x:
 		if expanded:
@@ -33,6 +45,57 @@ func _process(_delta):
 		if not expanded:
 			animation.play_backwards("retract")
 			expanded = true
+			
+	if music_playing:
+		var position = AUDIO.music.get_playback_position()
+		song_progress.value = position
+		var minutes = floor(song_progress.value/60)
+		var seconds = (song_progress.value/60 - minutes) * 60
+	
+		song_time.text = str("%01d:%02d") %[minutes,floor(seconds)]
+	
 
+func play_next():
+	song_index += 1
+	if song_index + 1 > music_playlist.size(): song_index = 0
+	song_time.text = "0:00"
+	AUDIO.play(music_playlist[song_index])
+	update_labels()
 
 	
+func play_previous():
+	song_index -= 1
+	if song_index < 0: song_index = 0
+	song_time.text = "0:00"
+	AUDIO.play(music_playlist[song_index])
+	update_labels()
+	
+	
+func update_labels():
+	song_progress.value = 0
+	song_length.text = "0:00"
+	
+	var length = AUDIO.music.stream.get_length()
+	song_progress.max_value = length
+	song_title.text = music_playlist[song_index]
+	
+	var minutes = floor(length/60)
+	var seconds = (length/60 - minutes) * 60
+	
+	song_length.text = str("%01d:%02d") %[minutes,floor(seconds)]
+
+
+func _on_NextSongButton_pressed():
+	play_next()
+
+func _on_PreSongButton_pressed():
+	play_previous()
+
+
+func _on_PlayButton_pressed():
+	if paused:
+		AUDIO.resume("music")
+		paused = false
+	else:
+		AUDIO.pause("music")
+		paused = true
