9a2b040457d36a154543af978a6505d20607c336
lgmeister <72280298+lgmeister@users.noreply.github.com>
2023-05-12 14:12:44 -0700

    Added custom sound button

---
+++ addons/my_custom_node/sound_button.gd
@@ -0,0 +1,13 @@
+tool
+extends Button
+
+
+func _enter_tree():
+	connect("button_down",self,"_button_down")
+	connect("button_up",self,"_button_up")
+
+func _button_down():
+	AUDIO.play("click_down")
+	
+func _button_up():
+	AUDIO.play("click_up")
