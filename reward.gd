b28b22f60aade9aea250cda40864435e85a18168
lgmeister <72280298+lgmeister@users.noreply.github.com>
2023-06-09 10:55:30 -0700

    Global Gold share

--- reward.gd
+++ reward.gd
@@ -13,32 +13,46 @@ var goat ## player goat
 ###ICONS###
 var money_icon = preload("res://visual/GUI/icons/individual_32x32/icon011.png")
 
-
+### Bool ###
 var collected = false
+var add_impulse = true
+
 var collect_amount = 0
 var collect_type
 var number
+var reward_type
 
 func _ready():
 	self.hide()
-	reward_type()
-		
-	
+	get_reward_type()
 	yield(get_tree().create_timer(number/float(4)),"timeout")
 	self.show()
-	apply_impulse(Vector2(0,10),Vector2(rand_range(-200,200),-500))
+	
+	if reward_type != "currancy":
+		rare_particles.emitting = true
+	
+	if add_impulse:
+		apply_impulse(Vector2(0,10),Vector2(rand_range(-200,200),-500))
+		
 	collect_area.monitorable = true
 	collect_area.monitoring = true
 	set_collision_mask_bit(4, true)
 	set_collision_layer_bit(4, true)
 	
+	if Global.in_battle:
+		set_collision_mask_bit(8,false)
+	
 	
 func _process(_delta):
 	rare_particles.rotation_degrees = -(self.rotation_degrees)
-	
 
-func reward_type():
-	if number == 0 or number == 1:
+
+func get_reward_type():
+	if reward_type == "currancy":
+		reward_texture.texture = money_icon
+		collect_amount = 100
+		collect_type = "Gold"		
+	elif number == 0 or number == 1:
 		reward_texture.texture = money_icon
 		collect_amount = 100
 		collect_type = "Gold"
@@ -47,16 +61,37 @@ func reward_type():
 func _on_collect_area_body_entered(body):
 	if "goat" in str(body):
 		if not collected:
+			AUDIO.play("pickup")
 			collected = true
+			set_collision_mask_bit(4, false)
+			set_collision_layer_bit(4, false)
+			set_collision_layer_bit(13, false)
+			set_collision_mask_bit(0, false)
+			set_collision_layer_bit(0, false)
 			animation.play("get")
+			
 			if collect_amount > 1:
 				get_label.text = "+" + (str(collect_amount) + " " + collect_type)
 			if collect_type == "Gold":
-				Global.currancy_1 += collect_amount
+				gold_gain(collect_amount)
 				HUD.tooltip_top("currancy",Global.currancy_1)
-				HUD.tooltip_top("money_add",str(Global.currancy_1) + " + " + str(collect_amount))
+				HUD.tooltip_top("money_add",str(Global.currancy_1) + " (+" + str(collect_amount) + ")")
+		
 			self.set_collision_layer_bit(4,false)
 			yield(animation,"animation_finished")
 			self.queue_free()
+			
+func gold_gain(amount):
+	var goat_number = get_tree().get_nodes_in_group("player").size()
+	var divide_even = amount/goat_number
+	var remainder = amount -  (int(divide_even) * goat_number)
+	
+	Global.currancy_1 += amount
+	
+	for goat in get_tree().get_nodes_in_group("player"):
+		goat.goat_gold += amount
+	
+	if remainder > 0:
+		Global.active_goat.goat_gold += remainder
 			
 			
